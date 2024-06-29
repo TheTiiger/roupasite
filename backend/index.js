71942aa78ece3d1802536c4e7ab5a2c009529bfc
Marco Silva <marcovieira12@sapo.pt>
2024-06-29 17:42:08 +0100

    mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const PORT = 5000;
@@ -8,6 +10,9 @@ app.get("/api/home", (req, res) => {
     res.json({ message: "Hello World!" });
 });
 
+mongoose.connect("mongodb+srv://ms8310737:yZTG8CK1JbVGvzga@cluster0.sapnfy9.mongodb.net/")
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.log('Failed to connect to MongoDB', error));
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
