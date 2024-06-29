23997b491dca10df63c7ef2380c8dc94fc7551a3
Marco Silva <marcovieira12@sapo.pt>
2024-06-30 00:30:11 +0100

    Site css

---
+++ frontend/src/app/homepage.tsx
@@ -0,0 +1,7 @@
+function homepage() {
+    return (
+        <div>
+            <h1>Homepage</h1>
+        </div>
+    );
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 
+
 function Page() {
 
   const [message, setMessage] = useState("Loading...");
