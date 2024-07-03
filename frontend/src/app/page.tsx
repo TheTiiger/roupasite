6eff5a3de84454069b0b6059566de5eec8c0be02
RubennFX <rubenyotb@gmail.com>
2024-07-03 14:14:49 +0100

    Atualizacoes CSS

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,58 +1,11 @@
 async function Page() {
 
   
-    const message = await (await fetch("http://localhost:5000/api/home")).json();
+    //const message = await (await fetch("http://localhost:5000/api/home")).json();
  
   return (
     <div>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
-      <h1>{message.message}</h1>
+      <h1>h1</h1>
     </div>
   );
 }
